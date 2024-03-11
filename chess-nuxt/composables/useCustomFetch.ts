00c1697fede8e9b206dc55054ccd6e79f8dc7571
Colin Faivre <colin.faivre@gmail.com>
2024-03-11 13:04:59 +0100

    feat(chess-front): :sparkles: add useCustomFetch to handle token

---
+++ chess-nuxt/composables/useCustomFetch.ts
@@ -0,0 +1,43 @@
+import type { UseFetchOptions } from '#app'
+import { defu } from 'defu'
+
+const token = process.client ? localStorage.getItem('token') : ''
+
+export function useCustomFetch<T>(
+    url: string | (() => string),
+    options: UseFetchOptions<T> = {}
+) {
+    // const userAuth = useCookie('token')
+    // const config = useRuntimeConfig()
+
+    const defaults: UseFetchOptions<T> = {
+        baseURL: `http://localhost:3000/`,
+        // baseURL: config.baseUrl ?? 'https://api.nuxt.com',
+        // this overrides the default key generation, which includes a hash of
+        // url, method, headers, etc. - this should be used with care as the key
+        // is how Nuxt decides how responses should be deduplicated between
+        // client and server
+        key: url,
+
+        // set user token if connected
+        // headers: userAuth.value
+        //     ? { Authorization: `Bearer ${userAuth.value}` }
+        //     : {},
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+
+        // onResponse(_ctx) {
+        //     // _ctx.response._data = new myBusinessResponse(_ctx.response._data)
+        // },
+
+        // onResponseError(_ctx) {
+        //     // throw new myBusinessError()
+        // },
+    }
+
+    // for nice deep defaults, please use unjs/defu
+    const params = defu(options, defaults)
+
+    return useFetch(url, params)
+}
