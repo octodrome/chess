df074e4f75a630313b04b4e5966ae1f1c2b2dbc6
Colin Faivre <colin.faivre@gmail.com>
2024-04-15 14:54:41 +0200

    feat: :sparkles: add linkedin link

--- chess-nuxt/components/App/Footer.vue
+++ chess-nuxt/components/App/Footer.vue
@@ -1,13 +1,21 @@
 <template>
     <footer class="p-2">
         <div class="text-center text-gray-500 w-full">
-            Made with ♥ by
+            --- Made with ♥ by
+            <a
+                href="https://www.linkedin.com/in/colin-faivre-020498136/"
+                target="_blank"
+                class="underline font-bold"
+                >Colin Faivre</a
+            >
+            @
             <a
                 href="https://github.com/octodrome"
                 target="_blank"
                 class="underline font-bold"
                 >Octodrome</a
             >
+            ---
         </div>
     </footer>
 </template>
