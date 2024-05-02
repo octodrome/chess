fea09a7b2799adabe6fee59b69987fb476745303
Colin Faivre <colin.faivre@gmail.com>
2024-05-02 16:55:43 +0200

    test: add test for createGame action

---
+++ chess-nuxt/stores/computerGameStore.spec.ts
@@ -0,0 +1,59 @@
+import { setActivePinia, createPinia } from 'pinia'
+import { useComputerGameStore } from './computerGameStore'
+import { vi, describe, it, expect, beforeEach, afterEach } from 'vitest'
+import type { IComputerGame } from '~/types/computerGame'
+import services from '~/services'
+
+vi.mock('../services/index.ts', () => {
+    return {
+        default: {
+            engine: {},
+            localGame: {
+                createLocalGame: vi.fn(() => Promise.resolve({}))
+            },
+            user: {},
+            game: {},
+            socket: {},
+        },
+    }
+})
+
+
+describe('ComputerGame Store', () => {
+    beforeEach(() => {
+        setActivePinia(createPinia())
+    })
+
+    it('has correct initial state', () => {
+        const computerGameStore = useComputerGameStore()
+        expect(computerGameStore.gameList).toStrictEqual([])
+        expect(computerGameStore.currentGame).toBe(null)
+    })
+
+    describe('actions', () => {
+        afterEach(() => {
+            vi.restoreAllMocks()
+        })
+
+        it('createGame()', async () => {
+            const fakeGame: IComputerGame = {
+                id: 'fake-id',
+                computerName: 'fake-name',
+                createdAt: new Date(),
+                updatedAt: new Date(),
+                playerColor: 'white',
+                computerLevel: 1,
+                fen: 'fake-fen',
+            }
+            services.localGame.createLocalGame.mockResolvedValue(fakeGame)
+
+            const computerGameStore = useComputerGameStore()
+            await computerGameStore.createGame({
+                playerColor: 'white',
+                computerLevel: 1,
+            })
+            expect(computerGameStore.currentGame).toStrictEqual(fakeGame)
+            expect(computerGameStore.gameList).toStrictEqual([fakeGame])
+        })
+    })
+})
