21c216b9c5c0ec4929c5dc101c206ff355db2352
Colin Faivre <colin.faivre@gmail.com>
2024-09-16 18:20:56 +0200

    feat(chess-back): :sparkles: add CORS middleware

--- go-rest-api-poc/main.go
+++ go-rest-api-poc/main.go
@@ -31,8 +31,25 @@ func loadEnv() {
 	}
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func serveApplication() {
 	router := gin.Default()
+	router.Use(CORSMiddleware())
 
 	publicRoutes := router.Group("/auth")
 	publicRoutes.POST("/register", controller.Register)
