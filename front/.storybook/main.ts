4496d309345b50acfdd0907fcdb0279d2b85cf8f
Colin Faivre <colin.faivre@gmail.com>
2024-10-16 17:00:34 +0200

    feat(front): :sparkles: add storybook and footer dummy story

---
+++ front/.storybook/main.ts
@@ -0,0 +1,29 @@
+import type { StorybookConfig } from '@storybook/vue3-vite'
+import { mergeConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+const config: StorybookConfig = {
+    stories: [
+        '../components/**/*.stories.@(js|ts)',
+    ],
+    addons: [
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions",
+        "@storybook/addon-links",
+    ],
+    framework: {
+        name: '@storybook/vue3-vite',
+        options: {},
+    },
+    docs: {
+        autodocs: 'tag',
+    },
+    async viteFinal(config, { configType }) {
+        // Merge custom Vite configuration with Storybook's
+        return mergeConfig(config, {
+            plugins: [vue()], // Add the Vue plugin for handling .vue files
+        });
+    },
+};
+
+export default config;
---
+++ front/components/App/AppFooter.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import AppFooter from './AppFooter.vue'
+
+// More on how to set up stories at: https://storybook.js.org/docs/vue/writing-stories/introduction
+
+const meta = {
+  title: 'App/AppFooter',
+  component: AppFooter,
+  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/vue/writing-docs/autodocs
+  tags: ['autodocs'],
+
+} satisfies Meta<typeof AppFooter>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const AppFooterStory: Story = {
+  args: {},
+}
