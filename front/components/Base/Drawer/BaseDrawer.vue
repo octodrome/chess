38f0fed62f162b342101247d6d0ae9d54fee8e52
Colin Faivre <colin.faivre@gmail.com>
2024-10-18 16:35:45 +0200

    fix(front): :bug: fix drawers overflow when too much content to present

--- front/components/Base/Drawer/BaseDrawer.vue
+++ front/components/Base/Drawer/BaseDrawer.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="bg-gray-800 text-gray-200 basis-80 shrink-0 drop-shadow-lg z-20"
+        class="bg-gray-800 text-gray-200 basis-80 shrink-0 drop-shadow-lg z-20 overflow-y-auto"
     >
         <slot />
     </div>
