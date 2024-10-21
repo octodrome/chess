08caa2c73a10c95b924f15a64f98425e72ae7260
Colin Faivre <colin.faivre@gmail.com>
2024-10-21 16:37:51 +0200

    fix(ci): :ambulance: fix

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
+
         baseUrl: process.env.BASE_URL || 'http://localhost:3000',
         video: false,
     },
