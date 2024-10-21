d57a1010a0c3f6072f79963157e3dd3619bc2053
Colin Faivre <colin.faivre@gmail.com>
2024-10-21 12:04:31 +0200

    chore(e2e): :building_construction: write a first working version of docker-compose.test to run e2e tests

--- e2e/cypress.config.js
+++ e2e/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
         setupNodeEvents(on, config) {
             // implement node event listeners here
         },
-        baseUrl: 'http://localhost:3000',
+        baseUrl: 'http://front:3000',
         video: false,
     },
 })
