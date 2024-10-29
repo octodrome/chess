93f7da5ace4dbe83389e80db0451e8c1b17f75ff
Colin Faivre <colin.faivre@gmail.com>
2024-10-29 10:12:58 +0100

    fix(front): :bug: make radio label clickable (#106)

--- front/components/Base/BaseRadio.vue
+++ front/components/Base/BaseRadio.vue
@@ -8,13 +8,15 @@ defineEmits(['update:modelValue'])
 </script>
 
 <template>
-    <input
-        type="radio"
-        :checked="props.modelValue === props.value"
-        :value="props.value"
-        name="pets"
-        v-bind="$attrs"
-        @change="$emit('update:modelValue', props.value)"
-    />
-    <label v-if="props.label" class="ml-2">{{ props.label }}</label>
+    <label>
+        <input
+            type="radio"
+            :checked="props.modelValue === props.value"
+            :value="props.value"
+            name="pets"
+            v-bind="$attrs"
+            @change="$emit('update:modelValue', props.value)"
+        />
+        <span v-if="props.label" class="ml-2">{{ props.label }}</span>
+    </label>
 </template>
