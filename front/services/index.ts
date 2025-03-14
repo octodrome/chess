2012a46d42419da6fddcc1410c65b624356bec26
Colin Faivre <colin.faivre@gmail.com>
2025-03-14 13:56:12 +0100

    refactor: remove go wasm poc

--- front/services/index.ts
+++ front/services/index.ts
@@ -2,12 +2,10 @@ import User from './api/User'
 import Engine from './engine'
 import Game from './api/Game'
 import LocalGame from './localGame/LocalGame'
-// import GoWasmPOC from './go-wasm-poc'
 
 export default {
     user: new User(),
     engine: new Engine(),
     game: new Game(),
     localGame: new LocalGame(),
-    // goWasmPOC: new GoWasmPOC(),
 }
